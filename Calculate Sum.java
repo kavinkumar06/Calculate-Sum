b6d424b345de71f7aff72509dc3d2a21ee62c702
kavinkumar06 <kavinm09@gmail.com>
2017-07-17 11:53:41 +0530

    Create Calculate Sum.java

---
+++ Calculate Sum.java
@@ -0,0 +1,14 @@
+# Calculate-Sum
+import java.io.*;
+import java.util.*;
+public class Calculate Sum
+{
+  public static void main(String args[])
+  {
+   int n; 
+  int s=0;
+  for(int i=2;i<=n;i+=2){
+   s+=i;
+ } 
+  return s;
+}
